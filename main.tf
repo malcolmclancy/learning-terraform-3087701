fbc980fb230e5c33ed2824d11955712c3d55b217
malcolmclancy <67573905+malcolmclancy@users.noreply.github.com>
2024-09-12 21:06:09 +1000

    load balancer

--- main.tf
+++ main.tf
@@ -36,23 +36,66 @@ module "blog_vpc" {
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
-
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   subnet_id = module.blog_vpc.public_subnets[0]
 
   tags = {
-    Name = "HelloWorld"
+    Name = "Learning Terraform"
+  }
+}
+
+module "alb" {
+  source = "terraform-aws-modules/alb/aws"
+
+  name    = "blog-alb"
+  vpc_id  = module.blog_vpc.vpc_id
+  subnets = module.blog_vpc.public_subnets
+  security_groups = module.blog_sg.security_group_id
+
+
+  listeners = {
+    ex-http-https-redirect = {
+      port     = 80
+      protocol = "HTTP"
+      redirect = {
+        port        = "443"
+        protocol    = "HTTPS"
+        status_code = "HTTP_301"
+      }
+    }
+    ex-https = {
+      port            = 443
+      protocol        = "HTTPS"
+      certificate_arn = "arn:aws:iam::123456789012:server-certificate/test_cert-123456789012"
+
+      forward = {
+        target_group_key = "ex-instance"
+      }
+    }
+  }
+
+  target_groups = {
+    ex-instance = {
+      name_prefix      = "blog"
+      protocol         = "HTTP"
+      port             = 80
+      target_type      = "instance"
+      target_id        = aws_instance.blog.id
+    }
+  }
+
+  tags = {
+    Environment = "dev"
   }
 }
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.2.0"
-  name = "blog"
-
+  
   vpc_id = module.blog_vpc.vpc_id
-
+  name = "blog"
   ingress_rules = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
