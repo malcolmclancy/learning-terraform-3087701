52c7be1deccae7e142cb0f911f096767a92f9ff8
malcolmclancy <67573905+malcolmclancy@users.noreply.github.com>
2024-09-12 21:20:13 +1000

    Changed listeners

--- main.tf
+++ main.tf
@@ -56,23 +56,10 @@ module "alb" {
 
 
   listeners = {
-    ex-http-https-redirect = {
+    http = {
       port     = 80
       protocol = "HTTP"
-      redirect = {
-        port        = "443"
-        protocol    = "HTTPS"
-        status_code = "HTTP_301"
-      }
-    }
-    ex-https = {
-      port            = 443
-      protocol        = "HTTPS"
-      certificate_arn = "arn:aws:iam::123456789012:server-certificate/test_cert-123456789012"
-
-      forward = {
-        target_group_key = "ex-instance"
-      }
+      target_groups = 0
     }
   }
 
