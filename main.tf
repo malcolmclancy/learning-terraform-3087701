0e2f0dafb6ff58ff13cbb28bc8f5cd6e6f72fe5d
malcolmclancy <67573905+malcolmclancy@users.noreply.github.com>
2024-09-14 22:01:29 +1000

    revert

--- main.tf
+++ main.tf
@@ -35,15 +35,14 @@ module "blog_vpc" {
 
 module "blog_autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
-  #version = "6.5.2"
-  version = "8.0.0"
+  version = "6.5.2"
 
   name = "blog"
 
   min_size            = 1
   max_size            = 2
   vpc_zone_identifier = module.blog_vpc.public_subnets
-  #target_group_arns   = module.blog_alb.target_group_arns
+  target_group_arns   = module.blog_alb.target_group_arns
   security_groups     = [module.blog_sg.security_group_id]
   instance_type       = var.instance_type
   image_id            = data.aws_ami.app_ami.id
@@ -51,8 +50,7 @@ module "blog_autoscaling" {
 
 module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
-  #version = "~> 6.0"
-  version = "9.11.0"
+  version = "~> 6.0"
 
   name = "blog-alb"
 
@@ -62,28 +60,7 @@ module "blog_alb" {
   subnets            = module.blog_vpc.public_subnets
   security_groups    = [module.blog_sg.security_group_id]
 
-  listeners = {
-    ex-http = {
-      port     = 80
-      protocol = "HTTP"
-
-      forward = {
-        target_group_key = "ex-blog-instance"
-      }
-    }
-  }
-  
-  target_groups = {
-    ex-blog-instance = {
-      name_prefix      = "blog-"
-      protocol         = "HTTP"
-      port             = 80
-      target_type      = "instance"
-      target_id        = (element(module.blog_alb,1)).id
-    }
-  }
-
-  /*target_groups = [
+  target_groups = [
     {
       name_prefix      = "blog-"
       backend_protocol = "HTTP"
@@ -98,7 +75,7 @@ module "blog_alb" {
       protocol           = "HTTP"
       target_group_index = 0
     }
-  ]*/
+  ]
 
   tags = {
     Environment = "dev"
