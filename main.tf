7acf53707f401d5e603632c2685cb31db0c4442d
malcolmclancy <67573905+malcolmclancy@users.noreply.github.com>
2024-09-14 21:43:06 +1000

    update to use module output

--- main.tf
+++ main.tf
@@ -75,7 +75,7 @@ module "blog_alb" {
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
-      target_id = data.blog_autoscaling.instance_id
+      target_id        = module.blog_autoscaling.instance_id
     }
   }
 
