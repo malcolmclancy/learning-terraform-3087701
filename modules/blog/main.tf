03bff0bb64fb03703476782ffdf50a73013b2563
malcolmclancy <67573905+malcolmclancy@users.noreply.github.com>
2024-10-01 12:46:32 +1000

    Update main.tf

--- modules/blog/main.tf
+++ modules/blog/main.tf
@@ -75,7 +75,7 @@ module "blog_alb" {
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
-      target_id        =  "vpc-XXXXXXXX"
+      target_id        =  module.blog_vpc.default_vpc_id
     }
   }
 
