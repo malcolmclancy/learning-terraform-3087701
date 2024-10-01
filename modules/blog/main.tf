4dcec1af1056ad57f924007ffd2f6eba375cc356
malcolmclancy <67573905+malcolmclancy@users.noreply.github.com>
2024-10-01 13:30:01 +1000

    Update main.tf

--- modules/blog/main.tf
+++ modules/blog/main.tf
@@ -42,7 +42,7 @@ module "blog_autoscaling" {
   min_size            = var.asg_min_size
   max_size            = var.asg_max_size
   vpc_zone_identifier = module.blog_vpc.public_subnets
-  target_group_arns   = [module.blog_alb.arn]
+  target_group_arns   = module.blog_alb.arn
   security_groups     = [module.blog_sg.security_group_id]
   instance_type       = var.instance_type
   image_id            = data.aws_ami.app_ami.id
